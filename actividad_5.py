5b99cc9b4ce7b0f65f5fd091165fc637a28e1392
Braulio Martinez Carrillo <braulio.martinez1138@alumnos.udg.mx>
2020-11-06 16:17:56 -0600

    Parte de busqueda hecha

--- actividad_5.py
+++ actividad_5.py
@@ -1,12 +1,19 @@
 import os
-from actividad_4 import Registro
+from registro import Registro
 
 with open("registros.txt", "r") as fich_regs:
     if os.path.exists("registros.txt"):
-        codigo_buscar = int(input("Ingrese el codigo a buscar: "))
-        linea = fich_regs.readline().split(",")
-        reg = Registro(int(linea[0]), linea[1], linea[2])
-        if reg.get_codigo() == codigo_buscar:
+        codigo_buscar = int(input("Codigo a buscar: "))
+        bandera = False
+        reg = Registro(0, "", "")
+        linea = fich_regs.readline()
+        while linea:
+            reg.from_file(linea.split(","))
+            if reg.get_codigo() == codigo_buscar:
+                bandera = True
+                break
+            linea = fich_regs.readline()
+        if bandera:
             print(reg.__str__())
         else:
             print("Registro no encontrado")
